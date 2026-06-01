f9fad15686acaf4a756db0582373734d0291ab4e
shizeying <shizeying@joyme.sg>
2026-06-01 22:11:19 +0800

    fix: update Entrox dev Homebrew formula

--- Formula/entrox.rb
+++ Formula/entrox.rb
@@ -4,21 +4,21 @@
 class Entrox < Formula
   desc "Entrox CLI"
   homepage "https://entrox.996icu.wiki"
-  version "0.0.0-ci.12.1"
+  version "0.0.0-ci.13.1"
   license "MIT"
 
   on_macos do
     depends_on arch: :arm64
 
     url "https://github.com/hexonal/entrox/releases/download/entrox-dev/entrox-cli-macos-arm64.zip"
-    sha256 "3e35ff8f5ab676d1a845f12f6151c12a8d0eab3c9b7e35387c6a2bf297f9a906"
+    sha256 "a5540b4eee1cdcf6be10ae6dd6bfffdbfe347ea2892cc75d7651d7bec11b34c7"
   end
 
   on_linux do
     depends_on arch: :x86_64
 
     url "https://github.com/hexonal/entrox/releases/download/entrox-dev/entrox-cli-linux-x64.zip"
-    sha256 "b6cab8e99ec6bb771b624b7e626eecc2a0765accfea6847a48a503fe448663aa"
+    sha256 "fa1808ae7c5ee79753c61335ef60e50b3a609bf65a933a3f294bc1300387c0e7"
   end
 
   depends_on "ripgrep"
