768cc3ceac9f4ab23d4ce7f2db04680bc81df1cd
shizeying <shizeying@joyme.sg>
2026-06-01 13:26:44 +0800

    Add Entrox dev formula

---
+++ Formula/entrox.rb
@@ -0,0 +1,29 @@
+# typed: false
+# frozen_string_literal: true
+
+class Entrox < Formula
+  desc "Entrox CLI"
+  homepage "https://entrox.996icu.wiki"
+  version "0.0.0-ci.4.1"
+  license "MIT"
+
+  on_macos do
+    url "https://github.com/hexonal/entrox/releases/download/entrox-dev/entrox-cli-macos-arm64.zip"
+    sha256 "55a63300eef99f0fa7ce04808df3be08c8c7cd86ef164ac227297e44b6c3c33e"
+  end
+
+  on_linux do
+    url "https://github.com/hexonal/entrox/releases/download/entrox-dev/entrox-cli-linux-x64.zip"
+    sha256 "8ee1884efb46cc3de0dddd150faf389f5ab36d0c36f04d363115ce98e58b56be"
+  end
+
+  depends_on "ripgrep"
+
+  def install
+    bin.install "bin/entrox"
+  end
+
+  test do
+    system "#{bin}/entrox", "--version"
+  end
+end
--- README.md
+++ README.md
@@ -1,3 +1,8 @@
 # Entrox Homebrew Tap
 
 This repository is managed by the Entrox release workflow.
+
+```bash
+brew tap hexonal/entrox
+brew install entrox
+```
